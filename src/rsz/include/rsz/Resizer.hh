cb266f1aaa4673c3c762387407d736d9aa6bd773
PrecisEDAnon <PrecisEDAnon@proton.me>
2025-12-29 05:29:06 +0000

    rsz: add optional RecoverPowerMore engine
    Signed-off-by: PrecisEDAnon <PrecisEDAnon@proton.me>

--- src/rsz/include/rsz/Resizer.hh
+++ src/rsz/include/rsz/Resizer.hh
@@ -64,6 +64,7 @@ using PinPtr = const sta::Pin*;
 using PinVector = std::vector<PinPtr>;
 
 class RecoverPower;
+class RecoverPowerMore;
 class RepairDesign;
 class RepairSetup;
 class RepairHold;
@@ -291,6 +292,8 @@ class Resizer : public sta::dbStaState, public sta::dbNetworkObserver
   void setMaxUtilization(double max_utilization);
   // Remove all or selected buffers from the netlist.
   void removeBuffers(sta::InstanceSeq insts);
+  bool removeBuffer(sta::Instance* buffer,
+                    bool honor_dont_touch_fixed = true);
   void unbufferNet(sta::Net* net);
   void bufferInputs(sta::LibertyCell* buffer_cell = nullptr,
                     bool verbose = false);
@@ -358,6 +361,7 @@ class Resizer : public sta::dbStaState, public sta::dbNetworkObserver
   bool recoverPower(float recover_power_percent,
                     bool match_cell_footprint,
                     bool verbose);
+  void setMoreRecoverPower(bool enable);
 
   ////////////////////////////////////////////////////////////////
   void swapArithModules(int path_count,
@@ -805,11 +809,14 @@ class Resizer : public sta::dbStaState, public sta::dbNetworkObserver
 
   // Components
   std::unique_ptr<RecoverPower> recover_power_;
+  std::unique_ptr<RecoverPowerMore> recover_power_more_;
   std::unique_ptr<RepairDesign> repair_design_;
   std::unique_ptr<RepairSetup> repair_setup_;
   std::unique_ptr<RepairHold> repair_hold_;
   std::unique_ptr<ConcreteSwapArithModules> swap_arith_modules_;
   std::unique_ptr<Rebuffer> rebuffer_;
+
+  bool more_recover_power_ = false;
 
   // Layer RC per wire length indexed by layer->getNumber(), corner->index
   sta::LibertyCellSet dont_use_;
@@ -932,6 +939,7 @@ class Resizer : public sta::dbStaState, public sta::dbNetworkObserver
   friend class GateCloner;
   friend class PreChecks;
   friend class RecoverPower;
+  friend class RecoverPowerMore;
   friend class RepairDesign;
   friend class RepairSetup;
   friend class RepairHold;
